2be692a7ab48d45e9aaec027ae02b61373c2706a
EC2 Default User <ec2-user@ip-172-31-3-84.ec2.internal>
2022-01-11 10:18:48 +0000

    [Add] 3-7 教材終了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 class ApplicationController < ActionController::Base
+
+  # devise_controller使用前に、実施する処理の条件
   before_action :configure_permited_parameters,if: :devise_controller?
 
-  protected
+  # サインイン後に遷移するページの指定
+  def after_sign_in_path_for(resourse)
+    about_path
+  end
 
+  # サインアウト後に遷移するページの指定
+  def after_sign_out_path_for(resourse)
+    about_path
+  end
+
+  protected
+  # ユーザ登録の際に、ユーザ名のデータ操作を許可をする。
   def configure_permited_parameters
     devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
   end
