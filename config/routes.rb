fe9fde9aaa6c2aba3e0348ef13ad138351bbefc2
EC2 Default User <ec2-user@ip-172-31-3-84.ec2.internal>
2022-01-12 04:05:22 +0000

    [Add] 4-9,4-10　教材終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-
-  devise_for :users
   root to: "homes#top" # ルートパスをtopアクションと紐付け
+  devise_for :users #deviceをしようする際にURLとして、usersを含むことを示す
+
+  #resourcesコマンドで自動生成するルーティングをonlyオプションで制限
+  resources :post_images,only:[:new,:create,:index,:show]
+
   get "homes/about" => "homes#about",as: "about"
+
 end
