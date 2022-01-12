4ca686f4badbb99c25a36a582d580b3024508ecb
EC2 Default User <ec2-user@ip-172-31-3-84.ec2.internal>
2022-01-12 07:57:45 +0000

    [Add] 4-15 教材終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # PostImageモデルとの関連付け
   has_many :post_images, dependent: :destroy
+  #ActiveStorageに"profile_image"という名前のプロフィール画像を保存する設定
+  has_one_attached :profile_image
+
+  def get_profile_image
+    unless profile_image.attached?
+      # プロフィール画像が存在しない場合
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path),filename:'default-image,jpg',content_type: 'image/jpeg')
+    end
+    # プロフィール画像が存在する場合
+    profile_image.variant(resize:"100x100").processed
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root to: "homes#top" # ルートパスをtopアクションと紐付け
-  devise_for :users #deviceをしようする際にURLとして、usersを含むことを示す
 
+  devise_for :users #deviceをしようする際にURLとして、usersを含むことを示す
+  root to: "homes#top" # ルートパスをtopアクションと紐付け
+  get "homes/about" => "homes#about",as: "about"
   #resourcesコマンドで自動生成するルーティングをonlyオプションで制限
   resources :post_images,only:[:new,:create,:index,:show,:destroy]
-
-  get "homes/about" => "homes#about",as: "about"
+  resources :users,only:[:show,:edit]
 
 end
